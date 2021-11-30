76bac488fc2e5ee181450836fc2ee7e09c58193f
MitkoDonev <donev90@gmail.com>
2021-11-30 18:30:00 +0200

    docker conf

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(1blo@v4p1%1a2#wh&2_*izsn%+@txy9rto7g3!2y@k*&#qvez"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=1))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
@@ -82,7 +83,7 @@ DATABASES = {
         "NAME": "dealership",
         "USER": "postgres",
         "PASSWORD": "Radina.987",
-        "HOST": "",
+        "HOST": "db",
         "PORT": "5432",
     }
 }
