5c03e1f446b59d78f2c3c50cef5a138c79c8cfce
MitkoDonev <donev90@gmail.com>
2021-11-30 11:51:03 +0200

    refactor views

--- app/dealership/urls.py
+++ app/dealership/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from .views import CarViewSet, TruckViewSet
 
-from .views import CarView, CarDetail, TruckView, TruckDetail
-
-urlpatterns = [
-    path('cars/', CarView.as_view()),
-    path('cars/<int:pk>/', CarDetail.as_view()),
-    path('trucks/', TruckView.as_view()),
-    path('trucks/<int:pk>/', TruckDetail.as_view()),
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
+router = DefaultRouter()
+router.register(r'cars', CarViewSet, basename='car')
+router.register(r'trucks', TruckViewSet, basename='truck')
+urlpatterns = router.urls
