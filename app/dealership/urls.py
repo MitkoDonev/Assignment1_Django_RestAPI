5d692e738c245b4a1294870d454237b58903099b
MitkoDonev <donev90@gmail.com>
2021-12-02 09:54:10 +0200

    refactor

--- app/dealership/urls.py
+++ app/dealership/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CarViewSet, TruckViewSet
 
 router = DefaultRouter()
+
 router.register(r'cars', CarViewSet, basename='car')
 router.register(r'trucks', TruckViewSet, basename='truck')
+
 urlpatterns = router.urls
